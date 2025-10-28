ba145f9f7a13dbae7a9bcb0979772dca441c3108
khushik17 <khushikabra2005@gmail.com>
2025-10-28 13:08:11 +0530

    fix: added socket

--- frontend/chatwebsite/src/socket.js
+++ frontend/chatwebsite/src/socket.js
@@ -1,5 +1,4 @@
 import { io } from "socket.io-client";
-
-const socket = io("http://localhost:3000"); // your backend URL
+const socket = io(import.meta.env.VITE_API_URL || "http://localhost:3000");
 
 export default socket;
